46b856a3631528d58e478c2e6f144f357b88765f
Raymond <rlin264@gmail.com>
2018-07-19 09:56:25 -0500

    resized corner detection box

--- preprocessing.py
+++ preprocessing.py
@@ -1,16 +1,17 @@
 import cv2
 import numpy as np
+from processing import Roi,corners
 
 def preprocessImage(image):
     (height, width, depth) = image.shape
     #image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    mask = np.zeros((height, width, 1), np.uint8)
+    #mask = np.zeros((height, width, 1), np.uint8)
     midHgt = height // 2
-    mask[midHgt:height, :, :] = 255
-    mask = cv2.merge((mask, mask, mask))
+    #mask[midHgt:height, :, :] = 255
+    #mask = cv2.merge((mask, mask, mask))
     print(image.shape)
-    maskedImage = cv2.bitwise_and(image, mask)
-    gImage = cv2.cvtColor(maskedImage, cv2.COLOR_BGR2GRAY)
+    #maskedImage = cv2.bitwise_and(image, mask)
+    gImage = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     ret, threshedMask = cv2.threshold(gImage, 180, 255, cv2.THRESH_BINARY)
     print(threshedMask.shape)
     threshedMask = cv2.merge((threshedMask, threshedMask, threshedMask))
@@ -21,8 +22,28 @@ def preprocessImage(image):
     return threshImage
 
 
+if __name__ == '__main__':
+    img = cv2.imread("test_images/solidYellowLeft.jpg")
 
-img = cv2.imread("test_images/solidYellowCurve2.jpg")
-cv2.imshow("orig", img)
-cv2.imshow("processed",preprocessImage(img))
-cv2.waitKey()
+    img, vertices = corners(img)
+    img = Roi(img, vertices)
+
+    print(vertices)
+
+    cv2.imshow("processed",preprocessImage(img))
+
+    cap = cv2.VideoCapture('test_videos/challenge.mp4')
+
+    while(cap.isOpened()):
+        ret, frame = cap.read()
+        if ret == True:
+            frame, vertices = corners(frame)
+            frame = Roi(frame, vertices)
+            cv2.imshow('Frame', frame)
+            if cv2.waitKey(25) & 0xFF == ord('q'):
+                break
+        else:
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
