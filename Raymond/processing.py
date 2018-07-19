0d99d069ad6cd0ff5f1f47765c4935d0f69f2e0a
Raymond Lin <rayster264@gmail.com>
2018-07-18 23:36:00 -0400

    WIP almost got edge of road detected

--- Raymond/processing.py
+++ Raymond/processing.py
@@ -7,9 +7,7 @@ def process(img):
     return
 
 
-def Roi(img):
-    height, width, depth = img.shape
-    vertices = [(0, height), (0, height / 2), (width / 2, height / 2), (width, height)]
+def Roi(img, vertices):
 
     mask = np.zeros_like(img)
     cv2.fillPoly(mask, np.array([vertices], dtype=np.int32), (255, 255, 255))
@@ -18,44 +16,56 @@ def Roi(img):
 
 
 def corners(img):
-    coord_lst = []
     height, width, depth = img.shape
+    coord_lst = [(width / 2, height / 2)]
 
-    grayImg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
     bot_right = img[height - 100:height, width - 100:width]
 
-    goodFeats = cv2.goodFeaturesToTrack(cv2.cvtColor(bot_right, cv2.COLOR_BGR2GRAY), 1, 0.10, 5)
-
-    for circleSet in goodFeats:
-        for circle in circleSet:
-            print(circle[0])
-            cv2.circle(bot_right, (int(circle[0]), int(circle[1])), 2, (0, 0, 255), -1)
+    dst = cv2.cornerHarris(cv2.cvtColor(bot_right, cv2.COLOR_BGR2GRAY), 2, 3, 0.04)
+    print(dst.max())
+    if dst.max() < 0.0001:
+        hgt, wid, dep = bot_right.shape
+        cv2.circle(bot_right, (wid, hgt), 3, (0, 0, 255), -1)
+        coord_lst.append([width,height])
+    else:
+        goodFeats = cv2.goodFeaturesToTrack(cv2.cvtColor(bot_right, cv2.COLOR_BGR2GRAY), 1, 0.10, 5)
+        for circleSet in goodFeats:
+            cv2.circle(bot_right, (circleSet[0][0], circleSet[0][1]), 3, (0, 0, 255), -1)
+        print(circleSet[0])
+        coord_lst.append([width-circleSet[0][0],height])
 
     bot_left = img[height - 100:, :100]
-    goodFeats1 = cv2.goodFeaturesToTrack(cv2.cvtColor(bot_left, cv2.COLOR_BGR2GRAY), 1, 0.10, 5)
-    # for circleSet in goodFeats1:
-    #     if type(circleSet) == None:
-    #         cv2.circle(bot_left, (0, height), 2, (0, 0, 255), -1)
-    #     else:
-    #         for circle in circleSet:
-    #             cv2.circle(bot_left, (circle[0], circle[1]), 2, (0, 0, 255), -1)
-    #             print(circle)
 
-    dst = cv2.cornerHarris(cv2.cvtColor(bot_right,cv2.COLOR_BGR2GRAY),2,3,0.04)
-    dst = cv2.dilate(dst,None)
-    print(dst.max()) #must be greater than 0.001 to be considered a corner
-    cv2.imshow("eigen",bot_left)
+    dst = cv2.cornerHarris(cv2.cvtColor(bot_left, cv2.COLOR_BGR2GRAY), 2, 3, 0.04)
+    hgt, wdt, dep = bot_left.shape
+    if dst.max() < 0.0001:
+        cv2.circle(bot_left, (0, hgt), 3, (0, 0, 255), -1)
+        coord_lst.append([0, height])
+    else:
+        goodFeats1 = cv2.goodFeaturesToTrack(cv2.cvtColor(bot_left, cv2.COLOR_BGR2GRAY), 1, 0.10, 5)
+        if goodFeats1 is not None:
+            for circleSet in goodFeats1:
+                cv2.circle(bot_left, (circleSet[0][0], circleSet[0][1]), 3, (0, 0, 255), -1)
+            coord_lst.append([circleSet[0][0],height])
+        else:
+            print("hi")
+            cv2.circle(bot_left, (0, hgt), 3, (0, 0, 255), -1)
+            coord_lst.append([0, height])
 
-    return img
+    print(coord_lst)
+
+    return img, coord_lst
 
 
 two_up = os.path.abspath(os.path.join(__file__, "../../test_images/solidWhiteCurve.jpg"))
 img = cv2.imread(two_up)
 print(img.shape)
 
-corner = corners(img)
+corner, vertices = corners(img)
 
-cv2.imshow("Image", corner)
+im1 = Roi(img, vertices)
+
+cv2.imshow("Image", im1)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
