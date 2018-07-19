d1d34eab96819a24ebd807879bd26f824c30aef0
AidanM11 <aidanmcvea@icloud.com>
2018-07-19 15:12:16 -0500

    reverting raymonds fuck up

--- preprocessing.py
+++ preprocessing.py
@@ -18,8 +18,8 @@ def preprocessImage(image):
     print(threshedMask.shape)
     threshedMask = cv2.merge((threshedMask, threshedMask, threshedMask))
     threshImage = cv2.bitwise_and(image, threshedMask)
-    threshImage = cv2.GaussianBlur(threshImage, (5,5), 0)
-    kern1 = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (2,15))
+    threshImage = cv2.GaussianBlur(threshImage, (3,5), 0)
+    kern1 = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (4,4))
     #threshImage = cv2.morphologyEx(threshImage, cv2.MORPH_CLOSE, kern1)
     return threshImage
 
