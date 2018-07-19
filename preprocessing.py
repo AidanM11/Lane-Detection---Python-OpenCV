489f8f07905c183eeef7d589d626c758cb5b86a8
Raymond <rlin264@gmail.com>
2018-07-19 15:10:28 -0500

    made corner detection dependent on image.shape

--- preprocessing.py
+++ preprocessing.py
@@ -1,26 +1,27 @@
 import cv2
 import numpy as np
-from processing import Roi,corners
+from processing import Roi, corners
+
 
 def preprocessImage(image):
     (height, width, depth) = image.shape
-    #image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    #mask = np.zeros((height, width, 1), np.uint8)
+    # image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # mask = np.zeros((height, width, 1), np.uint8)
     midHgt = height // 2
-    #mask[midHgt:height, :, :] = 255
-    #mask = cv2.merge((mask, mask, mask))
-    print(image.shape)
-    #maskedImage = cv2.bitwise_and(image, mask)
+    # mask[midHgt:height, :, :] = 255
+    # mask = cv2.merge((mask, mask, mask))
+    #print(image.shape)
+    # maskedImage = cv2.bitwise_and(image, mask)
     kern1 = cv2.getStructuringElement(cv2.MORPH_CROSS, (4, 4))
-    image = cv2.morphologyEx(image, cv2.MORPH_GRADIENT, kern1, iterations=2)
+    #image = cv2.morphologyEx(image, cv2.MORPH_GRADIENT, kern1, iterations=2)
     gImage = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     ret, threshedMask = cv2.threshold(gImage, 50, 255, cv2.THRESH_BINARY)
     print(threshedMask.shape)
     threshedMask = cv2.merge((threshedMask, threshedMask, threshedMask))
     threshImage = cv2.bitwise_and(image, threshedMask)
-    threshImage = cv2.GaussianBlur(threshImage, (5,5), 0)
-    kern1 = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (2,15))
-    #threshImage = cv2.morphologyEx(threshImage, cv2.MORPH_CLOSE, kern1)
+    threshImage = cv2.GaussianBlur(threshImage, (5, 5), 0)
+    kern1 = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (2, 15))
+    # threshImage = cv2.morphologyEx(threshImage, cv2.MORPH_CLOSE, kern1)
     return threshImage
 
 
@@ -30,25 +31,23 @@ if __name__ == '__main__':
     img, vertices = corners(img)
     img = Roi(img, vertices)
 
-    print(vertices)
+    #print(vertices)
 
-    cv2.imshow("processed",preprocessImage(img))
-    cv2.waitKey()
-    # cap = cv2.VideoCapture('test_videos/challenge.mp4')
-    #
-    # while(cap.isOpened()):
-    #     ret, frame = cap.read()
-    #     if ret == True:
-    #         frame, vertices = corners(frame)
-    #         frame = Roi(frame, vertices)
-    #         cv2.imshow('Frame', frame)
-    #         if cv2.waitKey(25) & 0xFF == ord('q'):
-    #             break
-    #         if cv2.waitKey(25) & 0xFF == ord(' '):
-    #             cv2.imwrite("image1.jpg", frame)
-    #
-    #     else:
-    #         break
-    #
-    # cap.release()
-    # cv2.destroyAllWindows()
+    cap = cv2.VideoCapture('test_videos/challenge.mp4')
+
+    while (cap.isOpened()):
+        ret, frame = cap.read()
+        if ret == True:
+            frame, vertices = corners(frame)
+            frame = Roi(frame, vertices)
+            cv2.imshow('Frame', frame)
+            if cv2.waitKey(25) & 0xFF == ord('q'):
+                break
+            if cv2.waitKey(25) & 0xFF == ord(' '):
+                cv2.imwrite("image1.jpg", frame)
+
+        else:
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
