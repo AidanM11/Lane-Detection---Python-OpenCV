08400775370f2ae20130298f343e7354328c73c3
Raymond <rlin264@gmail.com>
2018-07-20 14:05:16 -0500

    added histogram

--- processing.py
+++ processing.py
@@ -1,109 +1,102 @@
 import cv2
 import numpy as np
-import os
+
 
 def Roi(img, vertices):
-
     mask = np.zeros_like(img)
     cv2.fillPoly(mask, np.array([vertices], dtype=np.int32), (255, 255, 255))
     masked_image = cv2.bitwise_and(img, mask)
     return masked_image
 
 
-def corners(img):
-    height, width, depth = img.shape
-    print(img.shape)
-    coord_lst = [(width / 2 - int(width/2000), height / 2), (width / 2 + int(width/2000), height / 2)]
+def drawCorner(img, roi, coord_lst, dimensions, location):
+    hgt, wdt, dep = roi.shape
+    dst = cv2.cornerHarris(cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY), 2, 3, 0.04)
+    goodFeats = cv2.goodFeaturesToTrack(cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY), 1, 0.10, 5)
 
-    roi_h = int(height/10)
-    roi_w = int(width/8)
+    if location == 0:
+        coordinates = [dimensions[0], dimensions[1], 100]
+    elif location == 1:
+        coordinates = [dimensions[0], 0, -100]
 
-    bot_right = img[height - roi_h:height, width - roi_w:width]
-
-    dst = cv2.cornerHarris(cv2.cvtColor(bot_right, cv2.COLOR_BGR2GRAY), 2, 3, 0.04)
-    hgt, wdt, dep = bot_right.shape
-    goodFeats = cv2.goodFeaturesToTrack(cv2.cvtColor(bot_right, cv2.COLOR_BGR2GRAY), 1, 0.10, 5)
     if goodFeats is not None:
         if dst.max() < 0.0001:
-            cv2.circle(bot_right, (wdt, hgt), 5, (0, 255, 0), -1)
-            coord_lst.append([width, height])
+            cv2.circle(roi, (wdt, hgt), 5, (0, 255, 0), -1)
+            coord_lst.append([coordinates[1], coordinates[0]])
         else:
             for circleSet in goodFeats:
-                cv2.circle(bot_right, (circleSet[0][0], circleSet[0][1]), 5, (0, 255, 0), -1)
-                coord_lst.append([width - circleSet[0][0]+100,height])
+                cv2.circle(roi, (circleSet[0][0], circleSet[0][1]), 5, (0, 255, 0), -1)
+                coord_lst.append([coordinates[1] - circleSet[0][0] + coordinates[2], coordinates[0]])
 
     else:
-        cv2.circle(bot_right, (0, hgt), 5, (0, 255, 0), -1)
-        coord_lst.append([width, height])
-
-
-
-
-    bot_left = img[height - roi_h:height, 0:roi_w]
-
-    dst = cv2.cornerHarris(cv2.cvtColor(bot_left, cv2.COLOR_BGR2GRAY), 2, 3, 0.04)
-    hgt, wdt, dep = bot_left.shape
-    goodFeats1 = cv2.goodFeaturesToTrack(cv2.cvtColor(bot_left, cv2.COLOR_BGR2GRAY), 1, 0.10, 5)
-    if goodFeats1 is not None:
-        if dst.max() < 0.0001:
-            cv2.circle(bot_left, (0, hgt), 5, (0, 255, 0), -1)
-            coord_lst.append([0, height])
-        else:
-            for circleSet in goodFeats1:
-                cv2.circle(bot_left, (circleSet[0][0], circleSet[0][1]), 5, (0, 255, 0), -1)
-                coord_lst.append([circleSet[0][0]-200,height])
-
-    else:
-        cv2.circle(bot_left, (0, hgt), 5, (0, 255, 0), -1)
-        coord_lst.append([0, height])
+        cv2.circle(roi, (0, hgt), 5, (0, 255, 0), -1)
+        coord_lst.append([coordinates[1], coordinates[0]])
 
     return img, coord_lst
 
-def preprocess2(image):
 
+def corners(img):
+    dimensions = img.shape  # height, width depth
+    coord_lst = [(dimensions[1] / 2 - int(dimensions[1] / 20), int(3 * dimensions[0] / 5)),
+                 (dimensions[1] / 2 + int(dimensions[1] / 20), 3 * dimensions[0] // 5)]
+
+    roi_h = int(dimensions[0] / 10)
+    roi_w = int(dimensions[1] / 8)
+
+    bot_right = img[dimensions[0] - roi_h:dimensions[0], dimensions[1] - roi_w:dimensions[1]]
+
+    img, coord_lst = drawCorner(img, bot_right, coord_lst, dimensions, 0)
+
+    bot_left = img[dimensions[0] - roi_h:dimensions[0], 0:roi_w]
+
+    img, coord_lst = drawCorner(img, bot_left, coord_lst, dimensions, 1)
+
+    print(coord_lst)
+
+    return img, coord_lst
+
+
+def preprocess2(image):
     image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
-    thresh = 40
-    threshTowardsBrown = 2
-    thresh2 = 40
-
-    #bgr = [[0,255,255],[255,255,255]]
-    # hsv1 = cv2.cvtColor(np.uint8([[bgr[0]]]), cv2.COLOR_BGR2HSV)[0][0]
-    #hsv2 = cv2.cvtColor(np.uint8([[bgr[1]]]), cv2.COLOR_BGR2HSV)[0][0]
-
-    # maxHSV1 = np.array([hsv1[0] + thresh, hsv1[1] + 150, hsv1[2] + 150])
-    # minHSV1 = np.array([hsv1[0] - threshTowardsBrown, hsv1[1] - 10, hsv1[2] - 150])
     maxHSV1 = np.array([30, 255, 255])
     minHSV1 = np.array([17, 90, 70])
 
     maskHSV1 = cv2.inRange(image, minHSV1, maxHSV1)
     resultHSV1 = cv2.bitwise_and(image, image, mask=maskHSV1)
 
-    # maxHSV2 = np.array([hsv2[0]+150, hsv2[1]+150, hsv2[2]])
-    # minHSV2 = np.array([hsv2[0]-150, hsv2[1]-150, hsv2[2] - thresh2])
-    minHSV2 = np.array([0,0,90])
-    maxHSV2 = np.array([180,35,255])
+    minHSV2 = np.array([0, 0, 130])
+    maxHSV2 = np.array([180, 35, 255])
 
     maskHSV2 = cv2.inRange(image, minHSV2, maxHSV2)
     resultHSV2 = cv2.bitwise_and(image, image, mask=maskHSV2)
 
-    final_img = cv2.bitwise_or(resultHSV1,resultHSV2)
+    final_img = cv2.bitwise_or(resultHSV1, resultHSV2)
     final_img = cv2.cvtColor(final_img, cv2.COLOR_HSV2BGR)
+
+    return final_img
+
+def histogram(image):
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2YUV)
+    y, u, v = cv2.split(image)
+    equ = cv2.equalizeHist(y)
+
+    final_img = cv2.merge([equ,u,v])
+
+    #res = np.hstack((image, final_img))  # stacking images side-by-side
+
+    final_img = cv2.cvtColor(final_img, cv2.COLOR_YUV2BGR)
+
+    #cv2.namedWindow('res', cv2.WINDOW_AUTOSIZE)
+    #cv2.imshow('res', res)
 
 
     return final_img
 
-
-#two_up = os.path.abspath(os.path.join(__file__, "../../test_images/solidWhiteCurve.jpg"))
-# img = cv2.imread("test_images/solidYellowCurve2.jpg")
-# cv2.imshow("orig", img)
-#
-# print(img.shape)
-#
-# corner, vertices = corners(img)
-#
-# im1 = Roi(img, vertices)
-#
-# cv2.imshow("Image", im1)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
+if __name__ == "__main__":
+    img = cv2.imread("test_images/solidYellowCurve2.jpg")
+    img, vertices = corners(img)
+    img = Roi(img, vertices)
+    cv2.imshow("a", img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
