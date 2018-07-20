9b623965d3602e5a818a9975e91bf6605613e95a
AidanM11 <aidanmcvea@icloud.com>
2018-07-20 10:00:47 -0500

    retaining old, valid lines and reusing them

--- lineDetection.py
+++ lineDetection.py
@@ -4,7 +4,7 @@ from preprocessing import preprocessImage
 import processing
 
 
-def detectLines(image, origImg):
+def detectLines(image, origImg, defaultLine1, defaultLine2):
     image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     image = cv2.Canny(image, 20, 200)
     (height, width) = image.shape
@@ -15,8 +15,8 @@ def detectLines(image, origImg):
     #cv2.imshow("ROI", imageROI1)
     #cv2.imshow("ROI", imageROI2)
     #cv2.waitKey()
-    lines1 = cv2.HoughLinesP(imageROI1, 4, np.pi/180, 80, lines = 1, minLineLength=100, maxLineGap=600)
-    lines2 = cv2.HoughLinesP(imageROI2, 4, np.pi/180, 14, lines = 1, minLineLength=100, maxLineGap=600)
+    lines1 = cv2.HoughLinesP(imageROI1, 4, np.pi/180, 100, lines = 1, minLineLength=100, maxLineGap=600)
+    lines2 = cv2.HoughLinesP(imageROI2, 4, np.pi/180, 100, lines = 1, minLineLength=100, maxLineGap=600)
     image = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
     # if lines1 is not None:
     #     for lineSet in lines1:
@@ -42,6 +42,11 @@ def detectLines(image, origImg):
     points2y = []
     polyPoints = []
     bottomPointFound = False
+    lastGoodLine1 = defaultLine1
+    print("Last Good:" + (str)(lastGoodLine1))
+    lastGoodLine2 = defaultLine2
+    print("Last Good2:" + (str)(lastGoodLine2))
+
     if lines1 is not None:
         for lineSet in lines1:
             for line in lineSet:
@@ -53,6 +58,7 @@ def detectLines(image, origImg):
                     points1y.append(line[3])
         if len(points1x) > 0:
             m, b = np.polyfit(points1x, points1y, 1)
+            lastGoodLine1 = [m, b]
             allowedLength = (int)(width // 2 * 0.45)
             for x in range(width//2):
                 cv2.circle(origImg, (x, (int)(x*m + b)), 5, (0,0,255))
@@ -64,6 +70,34 @@ def detectLines(image, origImg):
                 if allowedLength == 0:
                     polyPoints.append([x, (int)(x*m + b)])
                     break
+        elif lastGoodLine1 is not None:
+            m = lastGoodLine1[0]
+            b = lastGoodLine1[1]
+            allowedLength = (int)(width // 2 * 0.45)
+            for x in range(width // 2):
+                cv2.circle(origImg, (x, (int)(x * m + b)), 5, (0, 0, 255))
+                if (int)(x * m + b) < height:
+                    allowedLength = allowedLength - 1
+                    if bottomPointFound == False:
+                        polyPoints.append([x, (int)(x * m + b)])
+                        bottomPointFound = True
+                if allowedLength == 0:
+                    polyPoints.append([x, (int)(x * m + b)])
+                    break
+    elif lastGoodLine1 is not None:
+        m = lastGoodLine1[0]
+        b = lastGoodLine1[1]
+        allowedLength = (int)(width // 2 * 0.45)
+        for x in range(width // 2):
+            cv2.circle(origImg, (x, (int)(x * m + b)), 5, (0, 0, 255))
+            if (int)(x * m + b) < height:
+                allowedLength = allowedLength - 1
+                if bottomPointFound == False:
+                    polyPoints.append([x, (int)(x * m + b)])
+                    bottomPointFound = True
+            if allowedLength == 0:
+                polyPoints.append([x, (int)(x * m + b)])
+                break
     bottomPointFound = False
     if lines2 is not None:
         for lineSet in lines2:
@@ -76,6 +110,7 @@ def detectLines(image, origImg):
                     points2y.append(line[3])
         if len(points2x) > 0:
             m, b = np.polyfit(points2x, points2y, 1)
+            lastGoodLine2 = [m, b]
             allowedLength = (int)(width // 2 * 0.45)
             for x in range(width//2):
                 x = width - x - width//2
@@ -88,17 +123,47 @@ def detectLines(image, origImg):
                 if allowedLength == 0:
                     polyPoints.append([x + width//2, (int)(x * m + b)])
                     break
+        elif lastGoodLine2 is not None:
+            print("Drawing old line")
+            m = lastGoodLine2[0]
+            b = lastGoodLine1[1]
+            allowedLength = (int)(width // 2 * 0.45)
+            for x in range(width // 2):
+                x = width - x - width // 2
+                cv2.circle(origImg, (x + width // 2, (int)(x * m + b)), 5, (0, 0, 255))
+                if (int)(x * m + b) < height:
+                    allowedLength = allowedLength - 1
+                    if bottomPointFound == False:
+                        polyPoints.append([x + width // 2, (int)(x * m + b)])
+                        bottomPointFound = True
+                if allowedLength == 0:
+                    polyPoints.append([x + width // 2, (int)(x * m + b)])
+                    break
+    elif lastGoodLine2 is not None:
+        print("Drawing old line")
+        m = lastGoodLine2[0]
+        b = lastGoodLine1[1]
+        allowedLength = (int)(width // 2 * 0.45)
+        for x in range(width // 2):
+            x = width - x - width // 2
+            cv2.circle(origImg, (x + width // 2, (int)(x * m + b)), 5, (0, 0, 255))
+            if (int)(x * m + b) < height:
+                allowedLength = allowedLength - 1
+                if bottomPointFound == False:
+                    polyPoints.append([x + width // 2, (int)(x * m + b)])
+                    bottomPointFound = True
+            if allowedLength == 0:
+                polyPoints.append([x + width // 2, (int)(x * m + b)])
+                break
     if len(polyPoints) == 4:
         temp = polyPoints[2]
         polyPoints[2] = polyPoints[3]
         polyPoints[3] = temp
-        print(polyPoints)
         polyPoints =np.array(polyPoints)
-        print(polyPoints)
         newImg = np.zeros_like(origImg)
         cv2.fillPoly(newImg, [polyPoints], (0,255,0))
         origImg = cv2.addWeighted(origImg, 1, newImg, 0.7, 0)
-    return origImg
+    return origImg, lastGoodLine1, lastGoodLine2
 
 def testLineDetect():
     img = cv2.imread("test_images/solidYellowLeft.jpg")
@@ -110,7 +175,9 @@ def testLineDetect():
     cv2.waitKey()
 
 def testLineVideo():
-    cap = cv2.VideoCapture("test_videos/solidWhiteRight.mp4")
+    cap = cv2.VideoCapture("test_videos/freewaybrent2_1.mp4")
+    lastLine1 = None
+    lastLine2 = None
     while cap.isOpened():
         ret, frame = cap.read()
         origFrame = frame
@@ -118,7 +185,7 @@ def testLineVideo():
         frame = preprocessImage(frame)
         frame, vertices = processing.corners(frame)
         frame = processing.Roi(frame, vertices)
-        origFrame = detectLines(frame, origFrame)
+        origFrame, lastLine1, lastLine2 = detectLines(frame, origFrame, lastLine1, lastLine2)
         cv2.imshow("Video", origFrame)
         cv2.imshow("Processed", frame)
         print("frame shown")
